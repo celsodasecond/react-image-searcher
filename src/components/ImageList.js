0cbd681f26607122f0a82bb607d12eff376d49fa
celsodasecond <75917932+celsodasecond@users.noreply.github.com>
2022-12-06 11:24:28 +0800

    ui changes

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 export default function ImageList( {images} ) {
     const imageOutput = images.map((image) => {
-        return <img key={image.id} src={image.webformatURL} alt="image" />
+        return (
+            <a  key={image.id} 
+                href={image.largeImageURL} 
+                class="ui medium image" 
+                style={{
+                    margin: '5px 5px 5px 5px', 
+                    float: 'left',
+                    width: '500px',
+                    height: 'auto',
+                    objectFit: 'cover', 
+                    }}>
+                <img src={image.webformatURL} />
+            </a>
+            
+        )
     })
     return (
         <div>
